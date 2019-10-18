49948bc39fac536b4e53f78a3dd537448a2efbff
kirillbeldyaga <kirill.beldyaga@darcmatter.com>
2019-10-18 16:49:36 +0300

    KB-33 Darc demom

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const DEFAULT_BECH32_PREFIX = 'darc';
-export const DEFAULT_DENOM = 'adarc';
+export const DEFAULT_DENOM = 'darc';
 export const DEFAULT_FEE = 0;
 export const DEFAULT_GAS = 200000;
 export const DEFAULT_KEY_PATH = 'm/44\'/118\'/0\'/0/0';
